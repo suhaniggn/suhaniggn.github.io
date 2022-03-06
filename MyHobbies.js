ad7db09d8298dcfc3fe16d4e9cba4468b2513ed9
suhani-g <101047492+suhani-g@users.noreply.github.com>
2022-03-06 22:37:54 +0530

    my html code

---
+++ MyHobbies.js
@@ -0,0 +1,20 @@
+//const badmintonPara = () => {
+//    const para = document.createElement('p')
+  //  para.innerHTML="<b>This is random text</b>";
+    //document.querySelector('body').appendChild(para)
+//}
+
+//const goHome = () => {
+    //const home = document.createElement('a')
+   // const link = document.createTextNode("Go To Homepage")
+   // home.appendChild(link); 
+    //home.title = "Go To Homepage";
+    //home.href = "Assignment.html"; 
+    
+//}
+
+let changeText = () => {
+    document.getElementById('intro').innerHTML = "<b>I'm 12, and I study in Amity International School, Sector 46, Gurgaon</b>" 
+}
+
+
